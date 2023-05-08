3b91cf1e750cea10bcab4a403775faa208f8cdb1
Veysel ŞİMŞİR <v.simsir@hotmail.com.tr>
2023-05-08 16:36:08 +0300

    vyselsmsr
    vyselsmsr

---
+++ food-ordering/pages/home/index.jsx
@@ -0,0 +1,22 @@
+import Campaigns from "@/components/Campaigns";
+import MenuWrapper from "@/components/product/MenuWrapper";
+import React from "react";
+import Carousel from "../../components/Carousel";
+import About from "../../components/About";
+import Reservation from "@/components/Reservation";
+import Customers from "@/components/customers/Customers";
+
+const Index = ({ categoryList,productList  }) => {
+  return (
+      <React.Fragment>
+        <Carousel />
+        <Campaigns />
+        <MenuWrapper categoryList={categoryList} productList={productList} />
+        <About />
+        <Reservation />
+        <Customers/>
+      </React.Fragment>
+  );
+};
+
+export default Index;
